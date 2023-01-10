1df79aa717e7c27ad6716d6aec9885cec869a2a5
Michell2022 <sistemas.angel04@gmail.com>
2023-01-10 13:20:33 -0500

    Logica terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+
 })
 export class HomeComponent {
 
+  public titulo = 'Data Binding Interpolation';
+
+  verOcultar = true;
+
   leerEntrada!:string;
+
+  textoInfo!:string;
+
+  // FUNCION PARA OCULTAR Y MOSTRAR + CAMBIO DE NOMBRE A UN BOTON PARA VER INFO 
+  buttonName = 'Ocultar';
+  cambiador = true;
+
+  toggle() {
+    
+    if (this.buttonName === 'Ocultar') {
+      this.cambiador=false;
+      this.buttonName = 'Mostrar';
+    } else if(this.buttonName === 'Mostrar') {
+      this.buttonName = 'Ocultar';
+      this.cambiador=true;
+    }
+  }
+
+
 }
