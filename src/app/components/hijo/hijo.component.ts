89a5fdadb913573e2b29b1ae675282ea71e86124
Michell2022 <sistemas.angel04@gmail.com>
2023-01-10 10:35:03 -0500

    logica terminada ppt14

---
+++ src/app/components/hijo/hijo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent {
+
+
+  @Input() 
+  textoRecibido = " ";
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { ComponentsModule } from '../components/components.module';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    FormsModule,
+    CommonModule, ComponentsModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class PagesModule { }
